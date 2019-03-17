0000e305b7ae55a8b784011fb099181f0a4c60c5
Xcfcdl <746953609@qq.com>
2019-03-17 17:05:54 +0800

    快速起步

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'model/post.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,14 +7,56 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Hello',style: TextStyle(fontSize: 25.0),),),
-        body:Hello()));
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.yellow),
+        home: Home());
   }
 }
 
 class Hello extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  Center(child: Text('Hello', textDirection: TextDirection.ltr, style: TextStyle(fontSize: 40.0,fontWeight: FontWeight.bold,color: Colors.yellow)));
-  }}
+    return Center(
+        child: Text('Hello',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.black)));
+  }
+}
+
+class Home extends StatelessWidget {
+  Widget _listItemBuilder(BuildContext context, int index) {
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          Image.network(posts[index].imageUrl),
+          SizedBox(height: 16.0,),
+          Text(posts[index].title,style: Theme.of(context).textTheme.title,),
+          Text(posts[index].author,style: Theme.of(context).textTheme.subhead,),
+          SizedBox(height: 16.0,),
+        ],
+      )
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          elevation: 8.0,
+          title: Text(
+            'Hello',
+            style: TextStyle(fontSize: 25.0),
+          ),
+        ),
+        body: ListView.builder(
+          itemCount: posts.length,
+          itemBuilder: _listItemBuilder,
+        ));
+  }
+}
